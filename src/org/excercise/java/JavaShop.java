c26b63f2fc89e2ff503b76b669b6b79ddec7117f
AimanFadil <aimanbello2001@gmail.com>
2024-05-17 16:20:50 +0200

    Java Shop

--- src/org/excercise/java/JavaShop.java
+++ src/org/excercise/java/JavaShop.java
@@ -1,5 +1,82 @@
 package org.excercise.java;
 
+import java.util.Random;
+
 public class JavaShop {
 
-}
+    //ATTRIBUTI
+
+
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+    private int iva;
+
+    //COSTRUTTORI
+
+    public JavaShop( String nome,String descrizione, double prezzo){
+        this.codice = getRandomCodice();
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = getPrezzoIva();
+    }
+
+
+
+    //METODI
+
+    String getCodiceStringa(){
+
+        String numeroStringa = Integer.toString(codice);
+        String addZero = "0000" + numeroStringa;
+        return addZero;
+    }
+
+    int getPrezzoIva(){
+        double aggiunta = prezzo * 0.22;
+        return (int) (prezzo + aggiunta);
+    }
+
+    int getRandomCodice(){
+        Random random = new Random();
+        int rand = random.nextInt(10000);
+        return rand;
+    }
+
+
+    //GETTER
+    public int getCodice() {
+        return codice;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+
+    //SETTER
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+   }
+
--- src/org/excercise/java/Main.java
+++ src/org/excercise/java/Main.java
@@ -1,7 +1,31 @@
 package org.excercise.java;
 
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
+
+        JavaShop prod2 = new JavaShop( "Giacca", "Giacca pelle", 12.90);
+        JavaShop prod3 = new JavaShop( "Pantalone", "Pantalone seta", 8.10);
+        JavaShop prod4 = new JavaShop( "Jeans", "Jeans Levis", 24.90);
+        JavaShop prod5 = new JavaShop( "Cappello", "Cappello NY", 19.90);
+        JavaShop prod6 = new JavaShop( "Scarpa", "Scarpa Nike", 30.00);
+        JavaShop prod7 = new JavaShop( "Camicia", "Camicia coreana", 11.90);
+
+
+        System.out.println("Codice prodotto: " + prod2.getCodiceStringa());
+        System.out.println("Prezzo prodotto: " +prod2.getPrezzo() + "$");
+        System.out.println("Prezzo con iva prodotto: " +prod2.getPrezzoIva() + "$");
+        System.out.println("Nome esteso: " + prod2.getCodice() + "-" +prod2.getNome() );
+
+
+
+
+
+
+
+
 
     }
 }
